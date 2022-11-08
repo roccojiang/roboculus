3748716504d9571ddd6ede0bc02ba76c3cd5b8bd
Andriy Vervega <anjey@dyn3212-31.wlan.ic.ac.uk>
2022-11-08 23:25:17 +0000

    fix: remove echoing data to client
    style: code formatting, naming variables

--- Assets/Scripts/Runtime/Server.cs
+++ Assets/Scripts/Runtime/Server.cs
@@ -7,84 +7,72 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Server : MonoBehaviour {
-
-  bool isFull;
-  int buffIndex;
-  ArrayList buffer;
+  private const int PORT = 5001;
+  private readonly string LOCAL_ADDR = GetLocalIPAddress();
+  private bool _isFull;
+  private int _buffIndex;
+  private ArrayList _buffer;
 
   // Start is called before the first frame update
   void Start() {
-    isFull = false;
-    buffIndex = 0;
-    buffer = new ArrayList(20);
-    Thread thread = new Thread(WaitForClient);
+    _isFull = false;
+    _buffIndex = 0;
+    _buffer = new ArrayList();
+    Thread thread = new Thread(AcceptRobotStateData);
     thread.Start();
   }
 
   // Update is called once per frame
   void Update() {
     // if there is data, read it
-    if (buffIndex < buffer.Count) {
-      var data = buffer[buffIndex];
-      // transform.Translate(Vector3.forward * (double) data);
-      buffIndex++;
+    if (_buffIndex < _buffer.Count) {
+      var data = _buffer[_buffIndex];
+      print(data);
+      transform.GetComponent<ArticulationBody>().TeleportRoot(
+          Vector3.forward * 0.5f, transform.rotation); //(double) data);
+      _buffIndex++;
     }
 
-    // Flush the buffer when it's full and we finished reading from it
-    if (isFull && buffIndex >= buffer.Count) {
-      print("Flushing the buffer");
-      buffer = new ArrayList(20);
-      buffIndex = 0;
-      isFull = false;
+    // Flush the _buffer when it's full and we finished reading from it
+    if (_isFull && _buffIndex >= _buffer.Count) {
+      print("Flushing the _buffer");
+      _buffer.Clear();
+      _buffIndex = 0;
+      _isFull = false;
     }
   }
 
   // Runs a server, connects a client, gets data from the client,
-  // writes to the buffer (global ArrayList), sends data back to the client,
-  // disconnects the client, repeat.
-  void WaitForClient() {
-    string localAddr = "127.0.0.1";
-    int port = 5001;
+  // writes to the _buffer (global ArrayList)
+  private void AcceptRobotStateData() {
     TcpListener server = null;
     try {
-      server = new TcpListener(IPAddress.Parse(localAddr), port);
+      server = new TcpListener(IPAddress.Parse(LOCAL_ADDR), PORT);
 
       server.Start();
-      print("Server has started on " + localAddr + ":" + port);
-      Byte[] readBuffer = new Byte[1];
+      int nextByte;
+      print("Server has started on " + LOCAL_ADDR + ":" + PORT);
 
       while (true) {
-
         print("Waiting for a client connection");
         TcpClient client = server.AcceptTcpClient();
         print("A client is connected.");
 
-        if (!isFull) {
+        if (!_isFull) {
           // Get a stream object for reading and writing
           NetworkStream stream = client.GetStream();
-          // i to track number of chars read
-          int i;
-          // word will be appended with number until space, then parsed into
-          // double
-          string word = "";
-          // Loop to receive all the data sent by the client.
-          while ((i = stream.Read(readBuffer, 0, readBuffer.Length)) != 0) {
-            // Translate data bytes to an ASCII string.
-            string ch = System.Text.Encoding.ASCII.GetString(readBuffer, 0, i);
-            // Parsing of the char
-            if (ch == " ") {
-              buffer.Add(double.Parse(word));
-              word = "";
+          string coordinate = "";
+          while ((nextByte = stream.ReadByte()) != -1) {
+            if (nextByte == ' ') {
+              _buffer.Add(double.Parse(coordinate));
+              coordinate = "";
             } else {
-              word += ch;
+              coordinate += (char)nextByte;
             }
-            // reply with received data
-            byte[] msg = System.Text.Encoding.ASCII.GetBytes(ch);
-            stream.Write(msg, 0, msg.Length);
           }
-          isFull = true;
-          // Parsing the last word
-          buffer.Add(double.Parse(word));
+          // Parsing the last coordinate
+          _buffer.Add(double.Parse(coordinate));
+          _isFull = true;
         }
         client.Close();
         print("Client is disconnected");
@@ -94,5 +82,16 @@ public class Server : MonoBehaviour {
     } finally {
       server.Stop();
     }
+  }
+
+  private static string GetLocalIPAddress() {
+    var host = Dns.GetHostEntry(Dns.GetHostName());
+    foreach (var ip in host.AddressList) {
+      if (ip.AddressFamily == AddressFamily.InterNetwork) {
+        return ip.ToString();
+      }
+    }
+    throw new System.Exception(
+        "No network adapters with an IPv4 address in the system!");
   }
 }
