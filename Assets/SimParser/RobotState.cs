71ad27793aff3878d57714ccdbeeac47f2449c36
Abdullah, Fawwaz <muhammad-fawwaz.abdullah20@imperial.ac.uk>
2022-11-17 15:35:08 +0000

    merge: Added verification for joint count in the parser.

--- Assets/SimParser/RobotState.cs
+++ Assets/SimParser/RobotState.cs
@@ -8,6 +8,8 @@ namespace SimParser {
 /// </summary>
 // First assumption: all data are doubles.
 public struct RobotState {
+  public int Joints { get; private set; }
+
   public (double x, double y, double z) Position { get; private set; }
 
   public (double i, double j, double k) Orientation { get; private set; }
@@ -39,7 +41,20 @@ public struct RobotState {
                                                         z)));
 
   public override string ToString() {
-    return $"RobotState [{nameof(Position)}: {Position}, {nameof(Orientation)}: {Orientation}, {nameof(LinearVelocity)}: {LinearVelocity}, {nameof(AngularVelocity)}: {AngularVelocity}, {nameof(JointPositions)}: {string.Join(", ", JointPositions)}, {nameof(JointVelocities)}: {string.Join(", ", JointVelocities)}]";
+    return $"RobotState [{nameof(Joints)}: {Joints}, {nameof(Position)}: {Position}, {nameof(Orientation)}: {Orientation}, {nameof(LinearVelocity)}: {LinearVelocity}, {nameof(AngularVelocity)}: {AngularVelocity}, {nameof(JointPositions)}: {string.Join(", ", JointPositions)}, {nameof(JointVelocities)}: {string.Join(", ", JointVelocities)}]";
+  }
+
+  /// <summary>
+  /// Checks if the state is valid by checking the joint position bounds.
+  /// </summary>
+  /// <returns>Either a FormatException or the current instance.</returns>
+  public Either<FormatException, RobotState> IsProbablyValid() {
+    // For a state to be valid, all of the joint positions must be between -PI/2
+    // and PI/2.
+    return JointPositions.Any(d => d is<-Math.PI / 2 or> Math.PI / 2)
+               ? Either<FormatException, RobotState>.ToLeft(new FormatException(
+                     "Joint position is out of range, joint count is probably incorrect."))
+               : Either<FormatException, RobotState>.ToRight(this);
   }
 
   /// <summary>
@@ -52,42 +67,31 @@ public struct RobotState {
   // According to hexapod_env.py, the ordering should be...
   public static Either<string, RobotState> ParseRobotState(int joints,
                                                            Scanner scn) {
-    // Pre-initialise the robot state object.
-    RobotState state = new() { JointPositions = new List<double>(),
-                               JointVelocities = new List<double>() };
+    return scn.ParseManyToLF(DoubleParser).FlatMap(ed => {
+      // Pre-initialise the robot state object.
+      RobotState state = new() { Joints = joints };
 
-    // Parse state components one-by-one and insert into local copy of state.
-    return ParseCoordTriple(scn)
-        .FlatMap(pos => {
-          state.Position = pos;
-          return ParseCoordTriple(scn);
-        })
-        .FlatMap(ori => {
-          state.Orientation = ori;
-          return ParseCoordTriple(scn);
-        })
-        .FlatMap(lv => {
-          state.LinearVelocity = lv;
-          return ParseCoordTriple(scn);
-        })
-        .FlatMap(av => {
-          state.AngularVelocity = av;
-          return Enumerable.Range(0, joints)
-              .Select(
-                  _ => ParseDouble(scn))
-              .Sequence();
-        })
-        .FlatMap(jps => {
-          state.JointPositions.AddRange(jps);
-          return Enumerable.Range(0, joints)
-              .Select(
-                  _ => ParseDouble(scn))
-              .Sequence();
-        })
-        .FlatMap(jvs => {
-          state.JointVelocities.AddRange(jvs);
-          return Either<string, RobotState>.ToRight(state);
-        });
+      // Check if there are enough doubles.
+      List<double> doubles = ed.ToList();
+
+      if (doubles.Count <= 0) {
+        return Either<string, RobotState>.ToLeft("End of stream.");
+      } else if (doubles.Count != 12 + 2 * joints) {
+        int real = (doubles.Count - 12) / 2;
+        throw new FormatException(
+            $"This data is for a robot with the incorrect joint count! Expected data for {joints} joints, got data for {real} joints!");
+      }
+
+      // Otherwise, reinitialise everything.
+      state.Position = (doubles[0], doubles[1], doubles[2]);
+      state.Orientation = (doubles[3], doubles[4], doubles[5]);
+      state.LinearVelocity = (doubles[6], doubles[7], doubles[8]);
+      state.AngularVelocity = (doubles[9], doubles[10], doubles[11]);
+      state.JointPositions = doubles.GetRange(12, joints);
+      state.JointVelocities = doubles.GetRange(12 + joints, joints);
+
+      return Either<string, RobotState>.ToRight(state);
+    });
   }
 }
 }
