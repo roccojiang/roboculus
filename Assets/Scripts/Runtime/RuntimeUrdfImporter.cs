0f5c21df331cc11955861530ef2c3134fbcfd6e5
Rocco Jiang <rocco.jiang20@imperial.ac.uk>
2022-11-28 16:43:41 +0000

    Server/client improvements

--- Assets/Scripts/Runtime/RuntimeUrdfImporter.cs
+++ Assets/Scripts/Runtime/RuntimeUrdfImporter.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using Runtime;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using Unity.Robotics.UrdfImporter;
 using Unity.Robotics.UrdfImporter.Control;
 using UnityEngine;
@@ -10,102 +13,97 @@ using UnityEngine;
 /// To use, attach to a gameobject and use the GUI to load a URDF.
 ///
 /// Notes:
-/// 
+///
 /// - This component is for demonstration and testing only
 ///   and is not intended to be used as is in a final product.
-/// 
-/// - The runtime import feature is currently only functional in builds created using
-///   Mono scripting backend and will not fully work in standalone builds created with
-///   IL2CPP due to the dependency to Assimpnet plugin for loading collada meshes.
-///   However URDF files that only use STL format for both visual and collision meshes
-///   can still be imported in runtime in standalone IL2CPP builds.  
+///
+/// - The runtime import feature is currently only functional in builds created
+/// using
+///   Mono scripting backend and will not fully work in standalone builds
+///   created with IL2CPP due to the dependency to Assimpnet plugin for loading
+///   collada meshes. However URDF files that only use STL format for both
+///   visual and collision meshes can still be imported in runtime in standalone
+///   IL2CPP builds.
 /// </summary>
-public class RuntimeUrdfImporter : MonoBehaviour
-{
-    public string defaultUrdfFilepath = "";
-    public bool setImmovableLink = true;
-    public bool useVHACD = false; // this is not yet fully functional in runtime.
-    public bool showProgress = false; // this is not stable in runtime.
-    public bool clearOnLoad = false;
+public class RuntimeUrdfImporter : MonoBehaviour {
+  public string defaultUrdfFilepath = "";
+  public bool setImmovableLink = true;
+  public bool useVHACD = false; // this is not yet fully functional in runtime.
+  public bool showProgress = false; // this is not stable in runtime.
+  public bool clearOnLoad = false;
 
-    public string immovableLinkName = "base_link";
-    public GameObject currentRobot = null;
+  public string immovableLinkName = "base_link";
+  public GameObject currentRobot = null;
 
-    public void LoadUrdf(string urdfFilepath)
-    {
-        Debug.Log("[+] LOAD URDF: " + urdfFilepath);
-        if (string.IsNullOrEmpty(urdfFilepath))
-        {
-            return;
-        }
+  public void LoadUrdf(string urdfFilepath) {
+    Debug.Log("[+] LOAD URDF: " + urdfFilepath);
+    if (string.IsNullOrEmpty(urdfFilepath)) {
+      return;
+    }
 
-        // yield return null;
+    // yield return null;
 
-        ImportSettings settings = new ImportSettings
-        {
-            chosenAxis = ImportSettings.axisType.yAxis,
-            convexMethod = useVHACD ? ImportSettings.convexDecomposer.vHACD : ImportSettings.convexDecomposer.unity,
-        };
+    ImportSettings settings = new ImportSettings {
+      chosenAxis = ImportSettings.axisType.yAxis,
+      convexMethod = useVHACD ? ImportSettings.convexDecomposer.vHACD
+                              : ImportSettings.convexDecomposer.unity,
+    };
 
-        GameObject robotObject = null;
-        robotObject = UrdfRobotExtensions.CreateRuntime(urdfFilepath, settings);
+    GameObject robotObject = null;
+    robotObject = UrdfRobotExtensions.CreateRuntime(urdfFilepath, settings);
 
-        if (robotObject != null && robotObject.transform != null) 
-        {
-            int joint_count = 18; // This will be parsed from urdf file instead
-            robotObject.transform.SetParent(transform);
-            SetControllerParameters(robotObject, joint_count);
-            Debug.Log("Successfully Loaded URDF" + robotObject.name);
-        }
+    if (robotObject != null && robotObject.transform != null) {
+      int jointCount = CountJoints(
+          urdfFilepath); // This will be parsed from the URDF file instead
+      robotObject.transform.SetParent(transform);
+      SetControllerParameters(robotObject, jointCount);
+      Debug.Log("Successfully Loaded URDF " + robotObject.name);
+    }
 
-        // clear the existing robot to avoid collision
-        if (clearOnLoad && currentRobot != null)
-        {
-          currentRobot.SetActive(false);
-          Destroy(currentRobot);
-        }
+    // clear the existing robot to avoid collision
+    if (clearOnLoad && currentRobot != null) {
+      currentRobot.SetActive(false);
+      Destroy(currentRobot);
+    }
 
-        currentRobot = robotObject;
-        Server new_server = currentRobot.GetComponent<Server>();
-        new_server.StopServer();
-        new_server.Start();
-
+    currentRobot = robotObject;
+    Server newServer = currentRobot.GetComponent<Server>();
+    newServer.StopServer();
+    newServer.Start();
   }
 
-  void SetControllerParameters(GameObject robot, int joint_count)
-    {
-        Transform baseNode = robot.transform.Find(immovableLinkName);
-        if (baseNode && baseNode.TryGetComponent<ArticulationBody>(out ArticulationBody baseNodeAB))
-        {
-            baseNodeAB.immovable = true;
-            baseNodeAB.useGravity = false;
-        }
+  private int CountJoints(string filePath) {
+    string contents = File.ReadAllText(filePath, Encoding.UTF8);
+    return Regex.Matches(contents, @"<joint.*>").Count;
+  }
 
-        BoxCollider grabbableCollider = baseNode.gameObject.AddComponent<BoxCollider>();
-        grabbableCollider.center = Vector3.zero;
-        grabbableCollider.size = new Vector3(0.1f, 0.1f, 0.1f);
-
-        if (robot.TryGetComponent<Controller>(out Controller controller))
-        {
-            Destroy(controller);
-        }
-
-        RobotControl control = robot.AddComponent<RobotControl>();
-        control.jointCount = joint_count;
-
-        Server old_server = currentRobot.GetComponent<Server>();
-        Server new_server = robot.AddComponent<Server>();
-
-        System.Type type = old_server.GetType();
-        // Copied fields can be restricted with BindingFlags
-        System.Reflection.FieldInfo[] fields = type.GetFields();
-        foreach (System.Reflection.FieldInfo field in fields)
-        {
-          field.SetValue(new_server, field.GetValue(old_server));
-        }
+  void SetControllerParameters(GameObject robot, int jointCount) {
+    Transform baseNode = robot.transform.Find(immovableLinkName);
+    if (baseNode && baseNode.TryGetComponent(out ArticulationBody baseNodeAB)) {
+      baseNodeAB.immovable = true;
+      baseNodeAB.useGravity = false;
     }
 
-    void Start()
-    {
+    BoxCollider grabbableCollider =
+        baseNode.gameObject.AddComponent<BoxCollider>();
+    grabbableCollider.center = Vector3.zero;
+    grabbableCollider.size = new Vector3(0.1f, 0.1f, 0.1f);
+
+    if (robot.TryGetComponent<Controller>(out Controller controller)) {
+      Destroy(controller);
     }
+
+    RobotControl control = robot.AddComponent<RobotControl>();
+    control.jointCount = jointCount;
+
+    Server oldServer = currentRobot.GetComponent<Server>();
+    Server newServer = robot.AddComponent<Server>();
+
+    System.Type type = oldServer.GetType();
+    // Copied fields can be restricted with BindingFlags
+    System.Reflection.FieldInfo[] fields = type.GetFields();
+    foreach (System.Reflection.FieldInfo field in fields) {
+      field.SetValue(newServer, field.GetValue(oldServer));
+    }
+  }
 }
--- Assets/Scripts/Runtime/Server.cs
+++ Assets/Scripts/Runtime/Server.cs
@@ -5,128 +5,183 @@ using System.Net;
 using System.Collections.Concurrent;
 using System.Net.Sockets;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
 
 namespace Runtime {
-  public class Server : MonoBehaviour {
-    public int port;
-    public GameObject errorPopup;
-    public ErrorController errorController;
+public class Server : MonoBehaviour {
+  public int port;
+  public GameObject errorPopup;
+  public ErrorController errorController;
 
-    private readonly string LOCAL_ADDR = GetLocalIPAddress();
-    private IEnumerator<RobotState> _states;
-    private ConcurrentQueue<RobotState> _buffer;
-    private ConcurrentQueue<Exception> _threadException;
-    private RobotControl _control;
-    private TcpListener server;
+  private readonly string _localAddr = Utils.GetLocalIPAddress();
+  private IEnumerator<RobotState> _states;
+  private ConcurrentQueue<RobotState> _buffer;
+  private ConcurrentQueue<Exception> _threadException;
+  private RobotControl _control;
+  private TcpListener _server;
+  private IPAddress _responder;
 
-    // Start is called before the first frame update
-    public void Start() {
-      print("[+] Server start called");
-      _control = GetComponent<RobotControl>();
-      _buffer = new ConcurrentQueue<RobotState>();
-      _threadException = new ConcurrentQueue<Exception>();
+  // Start is called before the first frame update
+  public void Start() {
+    print("[+] Server start called");
+    _control = GetComponent<RobotControl>();
+    _buffer = new ConcurrentQueue<RobotState>();
+    _threadException = new ConcurrentQueue<Exception>();
 
-      Thread thread = new(AcceptRobotStateData);
-      thread.Start();
+    Thread thread = new(ServerLoop);
+    thread.Start();
+  }
+
+  // Update is called once per frame
+  void FixedUpdate() {
+    if (OVRInput.GetUp(OVRInput.Button.One)) {
+      Respond();
     }
 
-    // Update is called once per frame
-    void FixedUpdate() {
-      if (_threadException.TryDequeue(out Exception exc)) {
-        var ec = errorController.textField;
-        ec.text = exc.Message;
-        errorPopup.SetActive(true);
-      }
-
-      // if there is data, read it
-      if (!_buffer.TryDequeue(out RobotState data))
-        return;
-
-      // Double-check that the data is valid, in case any joint positions are out
-      // of range.
-      switch (data.IsProbablyValid()) {
-      case Left<FormatException, RobotState> l:
-        _buffer.Clear();
-        throw l.FromLeft();
-      case Right<FormatException, RobotState> r:
-        _control.SetState(r.FromRight());
-        break;
-      default:
-        throw new InvalidCastException("Not an Either.");
-      }
+    if (_threadException.TryDequeue(out Exception exc)) {
+      TextMeshProUGUI ec = errorController.textField;
+      ec.text = exc.Message;
+      errorPopup.SetActive(true);
     }
 
-    // Runs a server, connects a client, gets data from the client,
-    // writes to the _buffer (global ArrayList)
-    private void AcceptRobotStateData() {
-      server = new(IPAddress.Parse(LOCAL_ADDR), port);
-      try {
-        server.Start();
-        // int nextByte;
-        print("Server has started on " + LOCAL_ADDR + ":" + port);
-
-        while (true) {
-          print("Waiting for a client connection");
-          TcpClient currentClient = server.AcceptTcpClient();
-          NetworkStream currentStream = currentClient.GetStream();
-          print("[+] A client is connected to the movement server.");
-
-          IEnumerator<RobotState> states = null;
-          try {
-            states = new SimulationParser(_control.jointCount, currentStream)
-                         .GetEnumerator();
-            bool hasRead = false;
-
-            // Keep the connection alive until a state is read.
-            while (!hasRead) {
-              hasRead = states.MoveNext();
-              if (hasRead) {
-                RobotState st = states.Current;
-                st.IsFirst = true;
-              }
-            }
-
-            // XXX: See scanner for description of hack.
-            do {
-              _buffer.Enqueue(states.Current);
-            } while (states.MoveNext());
-          } catch (Exception e) {
-            _threadException.Enqueue(e);
-          }
-
-          states?.Dispose();
-          currentClient.Close();
-          print("A connection is closed.");
-        }
-      } catch (SocketException e) {
-        print("SocketException: " + e);
-      } finally {
-        print("[+] Server thread ending");
-        server.Stop();
-      }
+    // if there is data, read it
+    if (!_buffer.TryDequeue(out RobotState data)) {
+      _control.Grabbable = true;
+      return;
     }
 
-    private static string GetLocalIPAddress() {
-      var host = Dns.GetHostEntry(Dns.GetHostName());
-      foreach (var ip in host.AddressList) {
-        if (ip.AddressFamily == AddressFamily.InterNetwork) {
-          return ip.ToString();
-        }
-      }
+    // Make robot not grabbable.
+    _control.Grabbable = false;
 
-      throw new Exception(
-          "No network adapters with an IPv4 address in the system!");
-    }
-
-    private void OnDestroy()
-    {
-      server.Stop();
-    }
-
-    public void StopServer()
-    {
-      server.Stop();
+    // Double-check that the data is valid, in case any joint positions are out
+    // of range.
+    switch (data.IsProbablyValid()) {
+    case Left<FormatException, RobotState> l:
+      _buffer.Clear();
+      _threadException.Enqueue(l.FromLeft());
+      break;
+    case Right<FormatException, RobotState> r:
+      _control.SetState(r.FromRight());
+      break;
+    default:
+      throw new InvalidCastException("Not an Either.");
     }
   }
+
+  private bool ReadIPAddress(TcpListener server) {
+    TcpClient currentClient = server.AcceptTcpClient();
+    NetworkStream currentStream = currentClient.GetStream();
+    print("[+] A client is sending its IP to the server.");
+
+    try {
+      _responder = null;
+
+      byte[] buf = new byte[512];
+      if (currentStream.Read(buf, 0, 512) > 0) {
+        string maybeIp = Encoding.UTF8.GetString(buf);
+        if (IPAddress.TryParse(maybeIp, out IPAddress addr)) {
+          print("[+] IP read: " + maybeIp);
+          _responder = addr;
+
+          return true;
+        }
+
+        print("[+] Invalid IP: " + maybeIp);
+        return false;
+      } else {
+        print("[+] Read unsuccessful. Disconnecting.");
+        return false;
+      }
+    } catch (Exception e) {
+      _threadException.Enqueue(e);
+      return false;
+    } finally {
+      currentStream.Close();
+      currentClient.Close();
+    }
+  }
+
+  private void ReceiveStates(TcpListener server) {
+    TcpClient currentClient = server.AcceptTcpClient();
+    NetworkStream currentStream = currentClient.GetStream();
+    print("[+] A client is connected to the movement server.");
+
+    IEnumerator<RobotState> states = null;
+    try {
+      states = new SimulationParser(_control.jointCount, currentStream)
+                   .GetEnumerator();
+      bool hasRead = false;
+
+      // Keep the connection alive until a state is read.
+      while (!hasRead) {
+        hasRead = states.MoveNext();
+        if (!hasRead)
+          continue;
+
+        RobotState st = states.Current;
+        st.IsFirst = true;
+      }
+
+      // XXX: See scanner for description of hack.
+      do
+        _buffer.Enqueue(states.Current);
+      while (states.MoveNext());
+    } catch (Exception e) {
+      _threadException.Enqueue(e);
+    }
+
+    states?.Dispose();
+    currentClient.Close();
+    print("[+] A movement client connection is closed.");
+  }
+
+  // Runs a server, connects a client, gets data from the client,
+  // writes to the _buffer (global ArrayList)
+  private void ServerLoop() {
+    _server = new TcpListener(IPAddress.Parse(_localAddr), port);
+    try {
+      _server.Start();
+      print("Server has started on " + _localAddr + ":" + port);
+
+      while (true) {
+        print("Waiting for a client connection");
+        // The first connection that receives an IP address.
+        do {
+          print("[+] Waiting for IP.");
+        } while (!ReadIPAddress(_server));
+
+        // The second connection that receives the states.
+        ReceiveStates(_server);
+      }
+    } catch (SocketException e) {
+      print("SocketException: " + e);
+    } finally {
+      print("[+] Server thread ending");
+      StopServer();
+    }
+  }
+
+  private void Respond() {
+    if (_responder == null) {
+      print("[+] No IP to respond to. Returning.");
+      return;
+    }
+
+    print("[+] Connecting to " + _responder + " to ask for data.");
+    using Socket client =
+        new(_responder.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+    client.Connect(_responder, 5001);
+    print("[+] Success.");
+  }
+
+  public void StopServer() {
+    _server?.Stop();
+    _server = null;
+  }
+
+  private void OnDestroy() { StopServer(); }
+}
 }
