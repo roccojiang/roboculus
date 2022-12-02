0dbee07af0a28ce24646b91e8326b401c9098bd8
Rocco Jiang <rocco@roccojiang.com>
2022-12-02 13:30:00 +0000

    fix: make floor visible again and show IP in menu

--- Assets/Scripts/Runtime/Server.cs
+++ Assets/Scripts/Runtime/Server.cs
@@ -29,16 +29,6 @@ public class Server : MonoBehaviour {
     _buffer = new ConcurrentQueue<RobotState>();
     _threadException = new ConcurrentQueue<Exception>();
 
-    // if (_localAddr == "127.0.0.1") {
-    //   TriggerPopupWindow?.Invoke(
-    //       "Oculus does not seem to be connected to the Internet. Please check
-    //       your connection and restart the app.");
-    // } else {
-    //   TriggerPopupWindow?.Invoke("Oculus IP address is " + _localAddr +
-    //                              ". Please connect using the Python
-    //                              scripts!");
-    // }
-
     Thread thread = new(ServerLoop);
     thread.Start();
   }
@@ -164,10 +154,6 @@ public class Server : MonoBehaviour {
         do {
           print("[+] Waiting for IP.");
         } while (!ReadIPAddress(_server));
-
-        // TriggerPopupWindow?.Invoke(
-        //     "Client connected. Please press 'A' to start streaming simulated
-        //     movement data.");
 
         // The second connection that receives the states.
         ReceiveStates(_server);
