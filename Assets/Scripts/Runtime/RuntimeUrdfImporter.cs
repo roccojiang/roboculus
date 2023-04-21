c2f0db26365049951903e2d8a77308e662ae85c8
Hisarli <tilman.hisarli21@imperial.ac.uk>
2023-04-21 17:45:59 +0100

    Fixing movement with multiple objects

--- Assets/Scripts/Runtime/RuntimeUrdfImporter.cs
+++ Assets/Scripts/Runtime/RuntimeUrdfImporter.cs
@@ -36,7 +36,10 @@ public class RuntimeUrdfImporter : MonoBehaviour {
 
   public static event Action<GameObject, string> RuntimeRobotImported;
 
+  public int counter = 0;
+
   public void LoadUrdf(string urdfFilepath) {
+    counter++;
     Debug.Log("[+] LOAD URDF: " + urdfFilepath);
     if (string.IsNullOrEmpty(urdfFilepath)) {
       return;
@@ -57,12 +60,17 @@ public class RuntimeUrdfImporter : MonoBehaviour {
       int jointCount = CountJoints(
           urdfFilepath); // This will be parsed from the URDF file instead
       robotObject.transform.SetParent(transform);
-      SetControllerParameters(robotObject, jointCount);
+
+      // SetControllerParameters(robotObject, jointCount, urdfFilepath);
+      SetControllerParameters(robotObject, jointCount, urdfFilepath);
+
+      // robotObject.name = robotObject.name + counter.ToString();
+
       Debug.Log("Successfully Loaded URDF " + robotObject.name);
     }
 
     // clear the existing robot to avoid collision (works - comment this to load multiple thing, then may have server issue)
-    if (clearOnLoad && currentRobot != null) {
+    if (clearOnLoad && currentRobot.name == "hexapod") {
       // currentRobot.SetActive(false);
       // Destroy(currentRobot);
       Server server = currentRobot.GetComponent<Server>();
@@ -73,6 +81,7 @@ public class RuntimeUrdfImporter : MonoBehaviour {
     currentRobot = robotObject;
     RuntimeRobotImported?.Invoke(currentRobot, immovableLinkName);
 
+    // Can try to comment out if server started in params
     Server newServer = currentRobot.GetComponent<Server>();
     newServer.StopServer();
     newServer.Start();
@@ -83,7 +92,8 @@ public class RuntimeUrdfImporter : MonoBehaviour {
     return Regex.Matches(contents, @"<joint.*>").Count;
   }
 
-  void SetControllerParameters(GameObject robot, int jointCount) {
+  // string urdfFilepath as third param
+  void SetControllerParameters(GameObject robot, int jointCount, string urdfFilepath) {
     ArticulationBody baseNode = robot.GetComponentInChildren<ArticulationBody>();
     if (baseNode) {
       baseNode.immovable = true;
@@ -95,11 +105,11 @@ public class RuntimeUrdfImporter : MonoBehaviour {
       grabbableCollider.size = new Vector3(0.1f, 0.1f, 0.1f);
 
       immovableLinkName = baseNode.gameObject.name;
-      // baseNode.gameObject.name = "BRO";  cannot do this as base_link is required to grab the object
+      // attach locateme tag to the object
       baseNode.gameObject.tag = "locateme";
     }
 
-    // The below lines on their own do not make the script run; also needs 64-67
+    // Destroy any pre-loaded controller if it exists
     if (robot.TryGetComponent<Controller>(out Controller controller)) {
       Destroy(controller);
     }
@@ -108,18 +118,30 @@ public class RuntimeUrdfImporter : MonoBehaviour {
     control.jointCount = jointCount;
 
     Server oldServer = currentRobot.GetComponent<Server>();
-    Server newServer = robot.AddComponent<Server>();
 
-    System.Type type = oldServer.GetType();
-    // Copied fields can be restricted with BindingFlags
-    System.Reflection.FieldInfo[] fields = type.GetFields();
-    foreach (System.Reflection.FieldInfo field in fields) {
-      field.SetValue(newServer, field.GetValue(oldServer));
-    }
+    if (urdfFilepath.Contains("bot")){
+      Server newServer = robot.AddComponent<Server>();
+
+      System.Type type = oldServer.GetType();
+      // Copied fields can be restricted with BindingFlags
+      System.Reflection.FieldInfo[] fields = type.GetFields();
+      foreach (System.Reflection.FieldInfo field in fields) {
+        field.SetValue(newServer, field.GetValue(oldServer));
+      }
+      newServer.StopServer();
+      newServer.Start();
+      
+      }
 
     foreach (ArticulationBody childBody in robot.GetComponentsInChildren<ArticulationBody>())
     {
       childBody.useGravity = false;
     }
+
+  //   if (urdfFilepath.Contains("bot")){
+  //     newServer.StopServer();
+  //     newServer.Start();
+  // }
+
   }
 }
